e03c24d33a11e6ef73cb0884da995d287f789f38
confleux <eternaldasein@gmail.com>
2022-01-31 01:36:27 +0300

    add http

--- index.js
+++ index.js
@@ -2,24 +2,29 @@
 
 const express = require('express');
 const https = require('https');
+const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const config = require('./lib/config.js');
 
 const app = express();
 
-const port = config.get('publicServerOptions').port;
+const httpsPort = config.get('publicServerOptions').httpsPort;
+const httpPort = config.get('publicServerOptions').httpPort;
+
+const options = {
+  key: fs.readFileSync(path.join(__dirname, config.get('privateServerOptions').sslKeyFilename)),
+  cert: fs.readFileSync(path.join(__dirname, config.get('privateServerOptions').sslCertFilename))
+}
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-https.createServer({
-      key: fs.readFileSync(path.join(__dirname, config.get('privateServerOptions').sslKeyFilename)),
-      cert: fs.readFileSync(path.join(__dirname, config.get('privateServerOptions').sslCertFilename))
-    },
-    app
-  )
-  .listen(port, () => {
-    console.log(`App started on port ${port}`);
-  });
+https.createServer(options, app).listen(httpsPort, () => {
+    console.log(`App https started on port ${httpsPort}`);
+});
+
+http.createServer(options, app).listen(httpPort, () => {
+    console.log(`App http started on port ${httpPort}`);
+});
