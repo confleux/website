600fbc9b684e1779b79f489a807c38af25dc1550
confleux <eternaldasein@gmail.com>
2022-01-30 18:52:27 +0300

    add https

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
 const path = require('path');
 
 const app = express();
@@ -9,6 +11,13 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`App started on port ${port}`);
-});
+https.createServer(
+  {
+    key: fs.readFileSync("server.key"),
+    cert: fs.readFileSync("server.cert")
+  },
+  app
+  )
+  .listen(port, () => {
+    console.log(`App started on port ${port}`);
+  });
